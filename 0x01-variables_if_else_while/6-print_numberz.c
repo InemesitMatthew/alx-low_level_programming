2184e66101917e0075af85c222a8ca6840a59eb5
Inemesit <inemesit.e.matthew@gmail.com>
2023-08-11 16:10:56 +0000

    6. Numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all single digit numbers of base 10,
+ *              starting from 0, followed by a new line.
+ *              You can only use the putchar function.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		putchar(digit + 48); // Adding 48 converts the digit to its ASCII character representation
+		digit++;
+	}
+
+	putchar('\n');
+
+	return (0);
+}
