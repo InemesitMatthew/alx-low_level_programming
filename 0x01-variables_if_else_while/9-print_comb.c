beacdfaeeab2fe8ea6431fc2d4fd1be4b51bf18f
Inemesit <inemesit.e.matthew@gmail.com>
2023-08-11 16:53:39 +0000

    9. Patience, persistence and perspiration make an unbeatable combination for success

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -4,28 +4,28 @@
  * main - Entry point
  *
  * Description: Prints all possible combinations of single-digit numbers,
- *              separated by ", ", in ascending order using the putchar function.
+ * separated by ", ", in ascending order using the putchar function.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    int digit = 0;
+	int digit = 0;
 
-    while (digit <= 9)
-    {
-        putchar(digit + '0');
+	while (digit <= 9)
+	{
+		putchar(digit + '0');
 
-        if (digit < 9)
-        {
-            putchar(',');
-            putchar(' ');
-        }
+		if (digit < 9)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 
-        digit++;
-    }
+		digit++;
+	}
 
-    putchar('\n');
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
