7e91a5a32aa0913dc637a091c749c9e592038b3a
Inemesit <inemesit.e.matthew@gmail.com>
2023-08-16 14:34:43 +0000

    Task 1 and subsequent files

---
+++ 0x02-functions_nested_loops/main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	/* Call the function to print the alphabet */
+	print_alphabet();
+
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,7 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Function Prototype */
+/* Function Prototype Putchar */
 int _putchar(char c);
+
+/* Prototype for print_alphabet */
+void print_alphabet(void);
 
 #endif /* MAIN_H */
