da19e1ffdb96e894b0560e8637f5648eba393ec1
Inemesit <inemesit.e.matthew@gmail.com>
2023-08-16 16:26:46 +0000

    To be finished i.e task 2

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,10 +1,11 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Function Prototype Putchar */
+/* Function Prototype */
 int _putchar(char c);
 
 /* Prototype for print_alphabet */
 void print_alphabet(void);
+void print_alphabet_x10(void);
 
 #endif /* MAIN_H */
