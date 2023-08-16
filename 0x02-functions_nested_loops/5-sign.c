186ace87517800166e1731e336ec5b27e4f12b17
Inemesit <inemesit.e.matthew@gmail.com>
2023-08-16 22:15:52 +0000

    Task 5. Sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,6 +1,27 @@
-#include "5-sign.h"
+#include "main.h"
 
+/**
+ * print_sign - Prints the sign of a number.
+ * @n: The number to be checked.
+ *
+ * Return: 1 if n is greater than zero,
+ * 0 if n is zero, -1 if n is less than zero.
+ */
 int print_sign(int n)
 {
-    /* Implementation of print_sign function */
+	if (n > 0)
+	{
+	_putchar('+');
+	return (1);
+	}
+	else if (n == 0)
+	{
+	_putchar('0');
+	return (0);
+	}
+	else
+	{
+	_putchar('-');
+	return (-1);
+	}
 }
--- 0x02-functions_nested_loops/5-sign.h
+++
@@ -1,6 +0,0 @@
-#ifndef SIGN_H
-#define SIGN_H
-
-int print_sign(int n);
-
-#endif /* SIGN_H */
