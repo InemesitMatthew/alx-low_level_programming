8c872bdbd55103b664bc568dee17e1c81a0b2868
Inemesit <inemesit.e.matthew@gmail.com>
2023-08-17 04:11:06 +0000

    Task 16

--- 0x02-functions_nested_loops/main.c
+++ 0x02-functions_nested_loops/main.c
@@ -1,7 +1,4 @@
 #include "main.h"
-#include "3-islower.h"
-#include "4-isalpha.h"
-#include "5-sign.h"
 
 /**
  * main - Entry point
@@ -10,29 +7,7 @@
  */
 int main(void)
 {
-	/*Task 1*/
-	/* Call the function to print the alphabet */
-	print_alphabet();
-
-	 /* Task 3: islower */
-    	int result_islower;
-    	char c_islower = 'a';
-    	result_islower = _islower(c_islower);
-    	_putchar(result_islower + '0');
-    	_putchar('\n');
-
-    	/* Task 4: isalpha */
-    	int result_isalpha;
-    	char c_isalpha = 'A';
-    	result_isalpha = _isalpha(c_isalpha);
-    	_putchar(result_isalpha + '0');
-    	_putchar('\n');
-
-  	/* Task 5: Sign */
-    	int n = -10;
-    	int result_sign = print_sign(n);
-    	_putchar(result_sign + '0');
-    	_putchar('\n');
+	void print_fibonacci();	
 
 	return (0);
 }
