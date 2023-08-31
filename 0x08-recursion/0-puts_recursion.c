837ee2847f52ec50e5644eea9dbb99ede93db119
Inemesit <inemesit.e.matthew@gmail.com>
2023-08-31 09:00:34 +0000

    Task 0: She locked away a secret, deep inside herself, something she once knew to be true... but chose to forget

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - Prints a string followed by a new line using recursion.
+ * @s: The string to be printed.
+ */
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+			 /**
+			  *Base case: Check if we've reached
+			  *the end of the string.
+			  */
+	{
+		_putchar('\n');/* Print a new line when we reach the end. */
+		return;
+	}
+	_putchar(*s);/* Print the current character. */
+	_puts_recursion(s + 1);
+			       /**
+				*  Recursively call the function
+				*  with the next character.
+				*/
+}
