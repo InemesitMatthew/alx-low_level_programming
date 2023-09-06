9e1fbd6e3a459e649b0911ecbda644103fe22cc8
Inemesit <inemesit.e.matthew@gmail.com>
2023-09-06 02:14:47 +0000

    Task 6: I will show you how great I am

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,101 +1,86 @@
 #include <stdlib.h>
+#include <stdio.h>
 
-int is_space(char c)
+/**
+ * duplicate_word - Duplicates a word from a string.
+ * @str: The string containing the word.
+ * @start: The starting index of the word.
+ * @end: The ending index of the word.
+ *
+ * Return: A newly allocated string containing the word.
+ */
+char *duplicate_word(char *str, int start, int end)
 {
-	return (c == ' ' || c == '\t' || c == '\n');
-}
+	int len = end - start + 1;
+	char *word = malloc(len + 1);
 
-int count_words(char *str)
-{
-	int count = 0, in_word = 0;
-
-	while (*str)
-	{
-		if (is_space(*str))
-		{
-			if (in_word)
-			{
-				in_word = 0;
-				count++;
-			}
-		}
-		else
-		{
-			in_word = 1;
-		}
-		str++;
-	}
-
-	if (in_word)
-		count++;
-
-	return (count);
-}
-
-char *duplicate_word(char *str)
-{
-	int len = 0;
-	char *word;
-
-	while (str[len] && !is_space(str[len]))
-		len++;
-
-	word = malloc(len + 1);
-
-	if (!word)
+	if (word == NULL)
 		return (NULL);
 
 	for (int i = 0; i < len; i++)
-		word[i] = str[i];
-
+		word[i] = str[start + i];
 	word[len] = '\0';
 
 	return (word);
 }
 
+/**
+ * strtow - Splits a string into words.
+ * @str: The input string.
+ *
+ * Return: An array of strings (words).
+ */
 char **strtow(char *str)
 {
-	int word_count;
-	char **words, *word;
-	char *str_copy = str;
+	int i, j, len = 0, word_count = 0;
+	char **words;
 
-	if (!str || !*str)
+	if (str == NULL || *str == '\0')
 		return (NULL);
 
-	word_count = count_words(str);
+	for (i = 0; str[i]; i++)
+	{
+		if (str[i] != ' ')
+        {
+		len++;
+		if (str[i + 1] == ' ' || str[i + 1] == '\0')
+		{
+			word_count++;
+			len = 0;
+		}
+	}
+	}
 
 	if (word_count == 0)
 		return (NULL);
 
 	words = malloc(sizeof(char *) * (word_count + 1));
 
-	if (!words)
+	if (words == NULL)
 		return (NULL);
 
-	int i;
-	for (i = 0; i < word_count; i++)
+	for (i = 0, j = 0; str[i]; i++)
 	{
-		while (*str_copy && is_space(*str_copy))
-			str_copy++;
+		if (str[i] != ' ')
+        {
+		int start = i;
 
-		word = duplicate_word(str_copy);
+		while (str[i] && str[i] != ' ')
+			i++;
 
-		if (!word)
+		words[j] = duplicate_word(str, start, i - 1);
+		if (words[j] == NULL)
 		{
-			for (int j = 0; j < i; j++)
-				free(words[j]);
-
+			while (j >= 0)
+				free(words[j--]);
 			free(words);
 			return (NULL);
 		}
 
-		words[i] = word;
-
-		while (*str_copy && !is_space(*str_copy))
-			str_copy++;
+		j++;
+	}
 	}
 
 	words[word_count] = NULL;
-
 	return (words);
 }
