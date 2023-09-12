955f59173431ae2edf768e44e2e7b80716163eb0
Inemesit <inemesit.e.matthew@gmail.com>
2023-09-12 14:36:03 +0000

    Task 1: A dog is the only thing on earth that loves you more than you love yourself

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,10 +1,22 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog {
+/**
+ * struct dog - Represents a dog
+ * @name: The dog's name
+ * @age: The dog's age
+ * @owner: The dog's owner
+ *
+ * Description: This structure represents information about a dog,
+ * including its name, age, and owner.
+ */
+struct dog
+{
 	char *name;
 	float age;
 	char *owner;
 };
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
 
 #endif
